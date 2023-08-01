a8635245f0a7aac35b952ff9f1562df0f630d56c
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-08-01 13:45:52 -0700

    initial commit of getkomora

---
+++ sled/getkomora.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# cache-advisor
+git clone git@github.com:komora-io/cache-advisor.git
+# concurrent-map
+git clone git@github.com:komora-io/concurrent-map.git
+# ebr
+git clone git@github.com:komora-io/ebr.git
+# fault-injection
+git clone git@github.com:komora-io/fault-injection.git
+# inline-array
+git clone git@github.com:komora-io/inline-array.git
+# pagetable
+git clone git@github.com:komora-io/pagetable.git
+# stack-map
+git clone git@github.com:komora-io/stack-map.git
