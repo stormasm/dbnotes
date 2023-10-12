2bb9cbbca017906031c2b80295bb2e64d7ac2d17
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-10-12 08:46:50 -0700

    mw

--- glaredb/gdb01.sh
+++ glaredb/gdb01.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-/Users/ma/j/tmp08/glaredb/target/debug/glaredb --query "create table person (id int, name string, primary key(id));  insert into person (id, name) values (1, 'Hopper'), (2, 'Kay'); select * from person;"
+/Users/ma/j/tmp08/glaredb/target/debug/glaredb --query "create table person (id int, name string, primary key(id));
+insert into person (id, name) values (1, 'Hopper'), (2, 'Kay');
+select * from person;"
